2aecf31834360125cc6bd7f0d011b1842570c7ca
Clayton Hill <clayman0123@yahoo.com>
2020-05-12 22:00:30 -0600

    updated routes from examples to todos

--- models/todo.js
+++ models/todo.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
+  var Todo = sequelize.define("Todo", {
     text: DataTypes.STRING,
     description: DataTypes.TEXT
   });
-  return Example;
+  return Todo;
 };
