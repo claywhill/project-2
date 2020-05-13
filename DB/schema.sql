c4cadddb5599055eeae01ee1b6dedfe272642cdc
MROD <mriguez777@gmail.com>
2020-05-12 21:18:03 -0600

    Created DB schema and seeds code. Schema creates the database and table. Seeds inserts data so that we have something to start with. Categories created in table: title, category, created and ETA.

--- DB/schema.sql
+++ DB/schema.sql
@@ -1,5 +1,17 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+DROP DATABASE IF EXISTS tbyd_db;
+CREATE DATABASE tbyd_db;
+
+USE tbyd_db;
+
+CREATE TABLE todo(
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(255),
+    category VARCHAR(255),
+    created DATE,
+    ETA DATE,
+    PRIMARY KEY(id)
+)
+
+SELECT * FROM todo;
+
---
+++ DB/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO todo (title, category, created, ETA) VALUES ("Trip to Spain", "Travel", '2020/05/12', '2025/05/12');
+INSERT INTO todo (title, category, created, ETA) VALUES ("Find a Hot Date", "Dating", '2020/05/12', '2020/12/12');
+INSERT INTO todo (title, category, created, ETA) VALUES ("Eat Puffer Fish", "Extreme", '2020/05/12', '2023/12/12');
